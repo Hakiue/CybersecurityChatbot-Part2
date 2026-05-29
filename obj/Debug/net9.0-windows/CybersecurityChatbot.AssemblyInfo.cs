69cb217a0b38005450a90f71f92d8bddf25333e0
Hakiue <zaakirshaibu71@gmail.com>
2026-05-29 21:29:58 +0200

    Removed build output files and added gitignore

--- obj/Debug/net9.0-windows/CybersecurityChatbot.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/CybersecurityChatbot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CybersecurityChatbot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac91efd22c12c4422dcaaacf4424dac59c29a038")]
 [assembly: System.Reflection.AssemblyProductAttribute("CybersecurityChatbot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CybersecurityChatbot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
